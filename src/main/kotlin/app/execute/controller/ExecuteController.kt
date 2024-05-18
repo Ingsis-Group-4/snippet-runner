8ebdef8dddc66ed8b36a96a7d278454f8e4b2f2b
Matías Bregoli <matias.bregoli@ing.austral.edu.ar>
2024-05-18 10:13:43 -0300

    Added swagger

--- src/main/kotlin/app/execute/controller/ExecuteController.kt
+++ src/main/kotlin/app/execute/controller/ExecuteController.kt
@@ -3,17 +3,13 @@ package app.execute.controller
 import app.execute.service.ExecuteOutput
 import app.execute.service.PrintScriptExecutor
 import jakarta.servlet.http.HttpServletRequest
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/execute")
-class ExecuteController {
+class ExecuteController : ExecuteControllerSpec {
     val printScriptExecutor = PrintScriptExecutor()
 
-    @PostMapping("")
-    private fun executeString(request: HttpServletRequest): ExecuteOutput {
+    override fun executeString(request: HttpServletRequest): ExecuteOutput {
         return printScriptExecutor.execute(request.inputStream)
     }
 }
